18b995866708add8bc549480715cc827d42b46f1
Bruno Fukumori <brunofukumori@gmail.com>
2026-08-31 12:53:08 -0300

    feat: remove Content Security Policy from next.config

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,6 @@
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === "development";
-
-const cspHeader = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline' https://va.vercel-scripts.com ${isDev ? "'unsafe-eval'" : ""};
-  style-src 'self' 'unsafe-inline';
-  img-src 'self' blob: data: https:;
-  font-src 'self' data: https://fonts.gstatic.com;
-  connect-src 'self' https://vitals.vercel-insights.com https://*.vercel-analytics.com;
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  upgrade-insecure-requests;
-`
-  .replace(/\s{2,}/g, " ")
-  .trim();
-
 const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: cspHeader,
-  },
   {
     key: "Strict-Transport-Security",
     value: "max-age=63072000; includeSubDomains; preload",
